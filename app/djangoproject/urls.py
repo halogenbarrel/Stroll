b2c2af2442e5ea2401209db562db4eaf35c45f22
Keaghon <keaghonc@gmail.com>
2025-11-06 13:43:26 -0600

    feat: can log into and out of superusers

--- app/djangoproject/urls.py
+++ app/djangoproject/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     
     # Authentication URLs
     path("login/", auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path("logout/", auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
