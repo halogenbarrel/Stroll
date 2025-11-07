0582e9bf54b391ee47c194882402453a305247a9
Claire Fuenning <cfuenning1@gmail.com>
2025-11-07 17:52:13 -0600

    added ability to create job post

--- app/job_board/forms.py
+++ app/job_board/forms.py
@@ -5,8 +5,12 @@ from userbase.models import Job
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = ['title', 'description', 'location', 'date', 'time', 'duration']
+        fields = [
+            'title', 'description', 'owner', 'dog', 'walker',
+            'scheduled_date', 'scheduled_time', 'duration',
+            'location', 'recurrence', 'status'
+        ]
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
-            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'scheduled_date': forms.DateInput(attrs={'type': 'date'}),
+            'scheduled_time': forms.TimeInput(attrs={'type': 'time'}),
         }
--- app/job_board/views.py
+++ app/job_board/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from userbase.models import Job
+from .forms import JobForm
 
 def job_detail(request, job_id):
     """
@@ -15,13 +16,22 @@ def job_list(request):
     Display all jobs from database
     TODO: only display jobs per owner/walker privilages
     """
-    jobs = Job.objects.filter(is_active=True)
+    jobs = Job.objects.filter(status__in=["OPEN","ASSIGNED"]) #shows all active jobs and excludes completed/cancelled jobs
     return render(request, 'job_board/job_list.html', {'jobs': jobs})
 
 
-'''
+
 def job_create(request):
     """
     Create a new job posting
     """
-'''
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('job_list')
+    else:
+        form = JobForm()
+    return render(request, 'job_board/create_job.html', {'form': form})
+
+
