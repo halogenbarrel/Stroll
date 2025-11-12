0c991e6ab99a3de4fa0323f18fbff96961729a71
Claire Fuenning <cfuenning1@gmail.com>
2025-11-11 20:21:56 -0600

    added owner_dashboard

--- app/dogs/urls.py
+++ app/dogs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dog_list, name='dog_list'),
     path('create/', views.create_dog, name='create_dog'),
+    path('dashboard/', views.owner_dashboard, name='owner_dashboard'),
 ]
