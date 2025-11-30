460740318bb670fa236e829eb18edf6d29797ee2
twistedttea <twistedtteavr@gmail.com>
2025-11-30 15:08:52 -0600

    Partial Revert "edit/delete dog profiles."
    This reverts commit b74c6457de423b3302ee576dc41d79509a1162f7.
    Drop dog_list.html

--- app/dogs/urls.py
+++ app/dogs/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', views.dog_list, name='dog_list'),
     path('create/', views.create_dog, name='create_dog'),
     path('dashboard/', views.owner_dashboard, name='owner_dashboard'),
-
-    path('<int:dog_id>/edit/', views.edit_dog, name='edit_dog'),
-    path('<int:dog_id>/delete/', views.delete_dog, name='delete_dog'),
 ]
