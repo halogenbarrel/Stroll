d6e90eed525feddc12ee1828299ee8590c60f710
Peter G. <twistedtteavr@gmail.com>
2025-12-01 21:33:42 -0600

    fix: dog thing lol

--- app/dogs/urls.py
+++ app/dogs/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dog_list, name='dog_list'),
     path('create/', views.create_dog, name='create_dog'),
+    path('<int:dog_id>/edit/', views.edit_dog, name='edit_dog'),
+    path('<int:dog_id>/delete/', views.delete_dog, name='delete_dog'),
     path('dashboard/', views.owner_dashboard, name='owner_dashboard'),
 ]
